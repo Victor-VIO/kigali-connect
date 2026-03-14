263c242095db53dc386010eb0dc5635de2a5ef1c
Victor <v.idowu@alustudent.com>
2026-03-14 13:13:18 +0200

    Final cleanup and remaining files

---
+++ lib/screens/home/main_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../../utils/app_colors.dart';
+import 'directory_screen.dart';
+import 'my_listings_screen.dart';
+import 'map_view_screen.dart';
+import 'settings_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = const [
+    DirectoryScreen(),
+    MyListingsScreen(),
+    MapViewScreen(),
+    SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: AppColors.primaryNavy,
+        selectedItemColor: AppColors.accentAmber,
+        unselectedItemColor: Colors.white.withAlpha(140),
+        elevation: 8,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_rounded),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt_rounded),
+            label: 'My Listings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map_rounded),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings_rounded),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
